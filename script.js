d12ca9c2361e5b3fac1ee1fe5990c746af473eb9
Akshay Chaudhari <akshaychaudhari1499@gmail.com>
2023-02-22 16:30:34 +0530

    completed FAQ , added toggle icon to FAQ

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 window.addEventListener('scroll',()=>{
     document.querySelector('nav').classList.toggle('window-scroll',window.scrollY>0)
 })
+
+
+// show / hide faq answer 
+const faqs=document.querySelectorAll('.faq')
+
+faqs.forEach(faq=>faq.addEventListener('click',()=>{
+    faq.classList.toggle('open');
+    
+    // to change the icon
+    const icon = faq.querySelector('.faq-icon i')
+    if (icon.className==='uil uil-plus'){
+        icon.className="uil uil-minus"}
+        else{
+            icon.className='uil uil-plus'
+        }
+}))
