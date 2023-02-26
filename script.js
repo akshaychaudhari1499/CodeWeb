60da216d039b1803c622bbdc837f3dba623b9b56
Akshay Chaudhari <akshaychaudhari1499@gmail.com>
2023-02-26 15:27:44 +0530

    footer,mediaqueri for tablets added

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 window.addEventListener('scroll',()=>{
     document.querySelector('nav').classList.toggle('window-scroll',window.scrollY>0)
 })
+menu=document.querySelector(".nav_menu");
+openbtn=document.querySelector("#open-menu");
+closebtn=document.querySelector("#close-menu");
+openbtn.addEventListener('click',()=>
+{
+    console.log('open-menu')
+    menu.style.display="flex";
+    openbtn.style.display="none";
+    closebtn.style.display="inline-block";
 
-
+})
+const closeNav=()=>{
+    console.log('open-menu')
+    menu.style.display="none";
+    openbtn.style.display="inline-block";
+    closebtn.style.display="none";
+}
+closebtn.addEventListener('click', closeNav)
 // show / hide faq answer 
 const faqs=document.querySelectorAll('.faq')
 
